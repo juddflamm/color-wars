6d1c4b15b911adacd2a3e6fe6311211277094302
Judd Flamm <kd7yva@gmail.com>
2015-09-28 10:55:01 -0600

    formatted angular only initial data map

--- angular-only/js/app.js
+++ angular-only/js/app.js
@@ -7,7 +7,12 @@ app.controller('Main', function($scope) {
   // The ID's of the squares
   $scope.bitIds = ['a0','a1','a2','a3','b0','b1','b2','b3','c0','c1','c2','c3','d0','d1','d2','d3'];
   // The color value for each square, by it's ID
-  $scope.bitMap = {'a0':'White','a1':'White','a2':'White','a3':'White','b0':'White','b1':'White','b2':'White','b3':'White','c0':'White','c1':'White','c2':'White','c3':'White','d0':'White','d1':'White','d2':'White','d3':'White'};
+  $scope.bitMap = {
+    'a0':'White','a1':'White','a2':'White','a3':'White',
+    'b0':'White','b1':'White','b2':'White','b3':'White',
+    'c0':'White','c1':'White','c2':'White','c3':'White',
+    'd0':'White','d1':'White','d2':'White','d3':'White'
+  };
 
   $scope.calcBitStyle = function(bitId) {
     var styles = {};

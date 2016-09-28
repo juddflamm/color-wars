004d493311554571f0749958f620b29cacbea00e
Judd Flamm <kd7yva@gmail.com>
2016-09-28 13:46:05 -0400

    AngularFire2 Updates

--- colorwars-angularfire2/src/app/app.component.ts
+++ colorwars-angularfire2/src/app/app.component.ts
@@ -9,10 +9,31 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class AppComponent {
 
-  title = 'app works!';
+  scoreWhite = 0;
+  scoreYellow = 0;
 
+  squares: FirebaseListObservable<any[]>;
   constructor(af: AngularFire) {
+    this.squares = af.database.list("/");
+    this.squares.subscribe(updatedSquares=>{
+      var newScoreWhite = 0;
+      var newScoreYellow = 0;
+      updatedSquares.forEach(updatedSquare => {
+        if (updatedSquare.color == "White") {
+          newScoreWhite++;
+        } else if (updatedSquare.color == "Yellow") {
+          newScoreYellow++;
+        }
+      });
+      this.scoreWhite = newScoreWhite;
+      this.scoreYellow = newScoreYellow;
+    })
 
+  }
+
+  squareClicked(square) {
+    var newVal = (square.color == "White")?"Yellow":"White";
+    this.squares.update(square.$key, {'color': newVal});
   }
 
 }
